f78af840cb1b4c7af609becb38a0517dc787fe02
Georgina Fraire Miranda <georgina.fraire@wizeline.com>
2022-06-14 16:47:23 -0500

    Filter list: componentDidUpdate

--- src/components/FilteredList.js
+++ src/components/FilteredList.js
@@ -1,22 +1,45 @@
 import React from "react";
-import PropsTypes from 'prop-types'
+import PropsTypes from "prop-types";
 
 export default class FilteredList extends React.Component {
+  static propsType = {
+    list: PropsTypes.arrayOf(PropsTypes.object).isRequiered,
+    filterValue: PropsTypes.string.isRequired,
+  };
 
-    static propsType = {
-        list: PropsTypes.arrayOf(PropsTypes.object).isRequiered,
-        filterValue: PropsTypes.string.isRequired
-    };
+  state = {
+    list: this.props.list,
+  };
 
-    render() {
-       return( 
-       <div>
-            { this.props.list.map((item) => {    
-              return (<div key={item.id}>
-                        <pre>{JSON.stringify(item, null, 2)}</pre>
-                    </div>)
-            })};
-        </div>
-        );
+  // method to filter the list, this is part of the component life cycle
+  componentDidUpdate(prevProps) {
+
+    if (prevProps.filterValue !== this.props.filterValue) {
+      this.setState({
+        list: this.props.list.filter((item) => {
+          return item.name
+            .toLowerCase()
+            .includes(this.props.filterValue.toLowerCase());
+        }),
+      }
+      );
     }
+   
+  }
+
+  render() {
+  
+    return (
+      <div>
+        {this.state.list.map((item) => {
+          return (
+            <div key={item.id}>
+              <pre>{JSON.stringify(item, null, 2)}</pre>
+            </div>
+          );
+        })}
+        ;
+      </div>
+    );
+  }
 }
